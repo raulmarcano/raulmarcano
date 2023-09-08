8479e89efa7867041ca1303063f2cbabfd56e373
Raúl Marcano <140550242+raulmarcano@users.noreply.github.com>
2023-09-08 14:54:56 +0200

    Primer commit

---
+++ app.js
@@ -0,0 +1,37 @@
+
+//FUNCIÓN PARA SCROLL AUTOMÁTICO
+
+var arrow = document.getElementById('arrow');
+var firstPage = document.getElementById('first-page');
+
+arrow.addEventListener('mouseover', function() {
+    
+    var windowHeight = document.documentElement.clientHeight;
+    
+    window.scrollBy({top:windowHeight, behavior:'smooth'});
+
+});
+
+//FUNCIÓN PARA DESVANECER FISRT PAGE
+
+window.addEventListener('scroll', function() {
+    
+    var scrollY = window.scrollY
+
+    var scrollclient = 200; 
+
+    if (scrollY < scrollclient) {
+        
+        firstPage.style.opacity = '1';
+    } else{
+        
+        firstPage.style.transition = 'opacity 0.2s ease';
+        firstPage.style.opacity = '0';
+    } 
+});
+
+
+
+
+
+
